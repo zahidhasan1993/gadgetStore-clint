a6b7beb3678089b812d79ad3394216e8ebf206e1
devZahid <officialzahidhasan19932021@gmail.com>
2024-01-25 13:54:02 +0600

    adding redux to state management for this products

---
+++ src/store.js
@@ -0,0 +1,17 @@
+import { applyMiddleware, combineReducers, createStore } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import { thunk } from "redux-thunk";
+import { productListReducer } from "./reducers/productReducres";
+
+const reducer = combineReducers({
+  productList: productListReducer,
+});
+const initialState = {};
+const middleware = [thunk];
+const store = createStore(
+  reducer,
+  initialState,
+  composeWithDevTools(applyMiddleware(...middleware))
+);
+
+export default store;
