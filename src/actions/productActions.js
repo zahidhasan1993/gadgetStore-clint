ad05fdd2b49102bd47511dc6e4662d1477f494a2
devZahid <officialzahidhasan19932021@gmail.com>
2024-01-25 14:07:32 +0600

    action and reducer added for the redux state management

---
+++ src/actions/productActions.js
@@ -0,0 +1,18 @@
+import { PRODUCT_LIST_FAIL, PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS } from "../contants/productConstants";
+
+
+export const listProducts = () => async (dispatch) => {
+    try {
+        dispatch({ type: PRODUCT_LIST_REQUEST })
+
+        const res = await fetch("http://localhost:3000/api/products");
+        const data = res.json();
+
+        dispatch({
+            type: PRODUCT_LIST_SUCCESS,
+            payload: data
+        })
+    } catch (error) {
+        dispatch({ type: PRODUCT_LIST_FAIL, payload: error.response && error.response.data.message ? error.response.data.message : error.message })
+    }
+}
