da28d0ece42d758151c52a3b4051d913d361f736
devZahid <officialzahidhasan19932021@gmail.com>
2024-01-25 16:15:47 +0600

    successfully getting product via redux store

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -6,17 +6,25 @@ import { listProducts } from "../../actions/productActions";
 const Home = () => {
   const dispatch = useDispatch();
   const productList = useSelector((state) => state.productList);
+
+  const { loading, error, products } = productList;
   useEffect(() => {
     dispatch(listProducts());
   }, [dispatch]);
 
   console.log(productList);
-  const products = [];
+
   return (
     <div>
       <main className="py-3">
         <Container>
-          <LatestProducts products={products}></LatestProducts>
+          {loading ? (
+            <h2>Loading</h2>
+          ) : error ? (
+            <h2>{error}</h2>
+          ) : (
+            <LatestProducts products={products}></LatestProducts>
+          )}
         </Container>
       </main>
     </div>
